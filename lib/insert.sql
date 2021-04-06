ddcf7cd2c398a659d283d1e6abc0c28b9b46a39d
Tim Quimbo <tquimbo1@gmail.com>
2021-04-05 21:45:49 -0400

    commit tree 375c2354ce71835020cfc56fe7619f2a3bc7abbb

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, color) VALUES ("Maru", 3, "Black");
+INSERT INTO bears (name, age, color) VALUES ("Baru", 4, "Red");
+INSERT INTO bears (name, age, color) VALUES ("Caru", 6, "Brown");
+INSERT INTO bears (name, age, color) VALUES ("Daru", 8, "Brown");
+INSERT INTO bears (name, age, color) VALUES ("Faru", 10, "Brown");
+INSERT INTO bears (name, age, color) VALUES ("Garu", 30, "Blue");
+INSERT INTO bears (name, age, color) VALUES ("Haru", 3, "Black");
+INSERT INTO bears (name, age, color) VALUES (NULL, 5, "White");
